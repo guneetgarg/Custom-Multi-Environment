f617a6773f30b85212ee1228f311e7f894a51b67
guneetgarg <guneetgarg@gmail.com>
2019-09-10 20:31:41 +0530

    Initial

--- src/test/java/functional/Demo1.java
+++ src/test/java/functional/Demo1.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 
 public class Demo1 {
 
-    public static void main(String[] args)  {
-        System.out.println(Config().data("url"));
-        System.out.println(Config().data("name.first"));
-        System.out.println(Config().data("name.first1"));
+    public static void main(String[] args) throws IOException {
+        System.out.println(Config.data("url"));
+        System.out.println(Config.data("name.first"));
+        System.out.println(Config.data("name.first1"));
     }
 }
